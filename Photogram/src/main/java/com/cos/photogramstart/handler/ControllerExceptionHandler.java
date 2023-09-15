a4f20ec967bdd0fe7f2701040303044daa38f1cd
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2023-09-15 18:44:38 +0900

    Script  통한 응답처리

--- Photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ Photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
+import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController
 @ControllerAdvice 
 public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(CustomValidationException.class) //RuntimeException 다 가로채서 가져옴
-	public Map<String,String> validationException(CustomValidationException e) {
-		return e.getErrorMap();
+	public String validationException(CustomValidationException e) {
+		//CMRespDto, Script 비교
+		//1. 클라이언트에게 응답할때는 Script 좋음
+		//2. Ajax 통신 - CMRespDto
+		//3. android - CMRespDto	
+		return Script.back(e.getErrorMap().toString());
 	}
 }
